0d14430a36fcfb7cbeb13b9116a8abe25a5640d3
pornvisa15 <pornvisa6568@gmail.com>
2024-11-15 14:04:58 +0700

    ppppp

--- login.php
+++ login.php
@@ -19,6 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
+
+    
     // ป้องกัน SQL Injection
     $user = $conn->real_escape_string($user);
     $pass = $conn->real_escape_string($pass);
