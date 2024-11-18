3157c1bafc71ab96fed4f441260428c1351f759f
Ploy1001 <ploy010445@gmail.com>
2024-11-18 16:35:12 +0700

    แก้แบบไม่แก้

--- pages/admin_homepages.php
+++ pages/admin_homepages.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,86 +12,89 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<!-- แถบที่1 -->
-<nav class="navbar navbar-dark">
-    <div class="container-fluid">
-        <span class="navbar-brand text-uppercase fw-bold fs-4" style="color: black;">ระบบ ยืม-คืน อุปกรณ์</span>
-        <div class="ms-auto">
-            <span class="text-lowercase" style="color: black;">แอดมิน</span>
+
+<body>
+
+    <!-- แถบที่1 -->
+    <nav class="navbar navbar-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand text-uppercase fw-bold fs-4" style="color: black;">ระบบ ยืม-คืน อุปกรณ์</span>
+            <div class="ms-auto">
+                <span class="text-lowercase" style="color: black;">แอดมิน</span>
+            </div>
         </div>
-    </div>
-</nav>
-<!-- แถบที่2 เขียว -->
-<nav class="navbar navbar-dark" style="background-color: #007468;">
-    <div class="container-fluid">
-        <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-white fw-bold">
-            <li><a href="#" class="nav-link px-2 text-white">หน้าหลัก</a></li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    คลังอุปกรณ์
-                </a>
-                <ul class="dropdown-menu text-white fw-bold" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">อุปกรณ์คอมพิวเตอร์</a></li>
-                    <li><a class="dropdown-item" href="#">อุปกรณ์วิทยาศาสตร์</a></li>
-                    <li><a class="dropdown-item" href="#">อุปกรณ์ดนตรี</a></li>
-                </ul>
-            </li>
+    </nav>
+    <!-- แถบที่2 เขียว -->
+    <nav class="navbar navbar-dark" style="background-color: #007468;">
+        <div class="container-fluid">
+            <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-white fw-bold">
+                <li><a href="#" class="nav-link px-2 text-white">หน้าหลัก</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        คลังอุปกรณ์
+                    </a>
+                    <ul class="dropdown-menu text-white fw-bold" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">อุปกรณ์คอมพิวเตอร์</a></li>
+                        <li><a class="dropdown-item" href="#">อุปกรณ์วิทยาศาสตร์</a></li>
+                        <li><a class="dropdown-item" href="#">อุปกรณ์ดนตรี</a></li>
+                    </ul>
+                </li>
 
-            <li><a href="#" class="nav-link px-2 text-white">ข้อมูลเจ้าหน้าที่</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">ประวัติการใช้อุปกรณ์</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">ออกจากระบบ</a></li>
-        </ul>
-    </div>
-</nav>
-<!-- แถบที่3 ขาว -->
-<nav class="navbar navbar-dark" style="background-color: #ffffff;">
-    <div class="container-fluid">
-        <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-black fw-bold">
-            <li><a href="#" class="nav-link px-2 text-white"></a></li>
+                <li><a href="#" class="nav-link px-2 text-white">ข้อมูลเจ้าหน้าที่</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">ประวัติการใช้อุปกรณ์</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">ออกจากระบบ</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- แถบที่3 ขาว -->
+    <nav class="navbar navbar-dark" style="background-color: #ffffff;">
+        <div class="container-fluid">
+            <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-black fw-bold">
+                <li><a href="#" class="nav-link px-2 text-white"></a></li>
 
-        </ul>
-    </div>
-</nav>
+            </ul>
+        </div>
+    </nav>
 
-<!-- แถบที่4  เทา-->
-<nav class="navbar navbar-dark" style="background-color:#c3cedc; border: 2px solid #616e8a; padding: 20px;">
-    <div class="container-fluid">
-        <span class="navbar-brand text-uppercase fw-bold fs-5" style="color: black;">รายละเอียดอุปกรณ์</span>
-    </div>
-</nav>
+    <!-- แถบที่4  เทา-->
+    <nav class="navbar navbar-dark" style="background-color:#c3cedc; border: 2px solid #616e8a; padding: 20px;">
+        <div class="container-fluid">
+            <span class="navbar-brand text-uppercase fw-bold fs-5" style="color: black;">รายละเอียดอุปกรณ์</span>
+        </div>
+    </nav>
 
-<!-- แถบที่5 ขาว -->
-<nav class="navbar navbar-dark" style="background-color: #ffffff;">
-    <div class="container-fluid">
-        <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-black fw-bold">
-            <li><a href="#" class="nav-link px-2 text-white"></a></li>
-        </ul>
-    </div>
-</nav>
+    <!-- แถบที่5 ขาว -->
+    <nav class="navbar navbar-dark" style="background-color: #ffffff;">
+        <div class="container-fluid">
+            <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-black fw-bold">
+                <li><a href="#" class="nav-link px-2 text-white"></a></li>
+            </ul>
+        </div>
+    </nav>
 
-<!-- Navbar ที่ 6 -->
-<nav class="navbar navbar-dark" style="background-color: #5AC9E4;">
-    <div class="container-fluid">
-        <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-white fw-bold">
-            <li><a href="#" class="nav-link px-6 text-white">ลำดับ</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">เลขพัสดุ/ครุภัณฑ์</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">ชื่ออุปกรณ์</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">วันที่ยืม</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">วันที่คืน</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">ผู้ยืม</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">สถานะ</a></li>
-            <li><a href="#" class="nav-link px-6 text-white">รายละเอียด</a></li>
-        </ul>
-    </div>
-</nav>
-
+    <!-- Navbar ที่ 6 -->
+    <nav class="navbar navbar-dark" style="background-color: #5AC9E4;">
+        <div class="container-fluid">
+            <ul class="nav col-10 col-lg-auto me-lg-auto mb-3 justify-content-center mb-md-0 text-white fw-bold">
+                <li><a href="#" class="nav-link px-6 text-white">ลำดับ</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">เลขพัสดุ/ครุภัณฑ์</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">ชื่ออุปกรณ์</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">วันที่ยืม</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">วันที่คืน</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">ผู้ยืม</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">สถานะ</a></li>
+                <li><a href="#" class="nav-link px-6 text-white">รายละเอียด</a></li>
+            </ul>
+        </div>
+    </nav>
 
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 
---
+++ pages/admin_homepages2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>สำรอง</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Sidebar -->
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark "
+        style="width: 220px; background-color: #5AC9E4; height: 100vh; position: fixed; left: 0; top: 0; justify-content-center">
+        <a
+            class="d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <span class="fw-bold fs-4">Admin</span>
+        </a>
+
+
+    </div>
+
+    <!-- Main content -->
+    <div class="container-fluid"></div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+</body>
+
+
+</html>
