774eda27c3e2b0f53ba793767ce74ac36832e8e5
pornvisa15 <pornvisa6568@gmail.com>
2024-12-17 13:12:53 +0700

    เอาใหม่

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -54,7 +54,23 @@ if (isset($_SESSION['officer_Right'])) {
 }    
 elseif($_SESSION['officer_Right'] == 3){
 ?>
-
+<div class="d-flex flex-column text-white p-4"
+        style="width: 250px; min-height: 100vh; background-color: #466da7;  margin-left: auto; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);">
+        <?php
+if (isset($_SESSION['officer_Right'])) {
+    if ($_SESSION['officer_Right'] == 3) { // ใช้ === เพื่อเปรียบเทียบค่า
+        echo "<h3 class='mb-4 text-center' style='background: rgba(0, 0, 0, 0.3); backdrop-filter: blur(5px); color: white; padding: 18px 20px; border-radius: 13px;'>แอดมิน</h3>";
+    } elseif ($_SESSION['officer_Right'] == 4) { // เปลี่ยนจาก officer_Cotton ให้ถูกต้อง
+        echo "<h3 class='mb-4 text-center' style='background: rgba(0, 0, 0, 0.3); backdrop-filter: blur(5px); color: white; padding: 18px 20px; border-radius: 13px;'>เจ้าหน้าที่</h3>";
+    } else {
+        echo "<span class='text-light'>ไม่มีสิทธิ์ </span>";
+     
+    }
+} else {
+    echo "<span class='text-light'>ไม่มีข้อมูลสิทธิ์</span>";
+   
+}
+?>   
 <ul class="nav flex-column">
             <li class="nav-item mb-3">
                 <a href="admin_homepages.php" class="nav-link text-white"
@@ -87,7 +103,7 @@ elseif($_SESSION['officer_Right'] == 3){
                 </a>
             </li>
         </ul>
-    
+        </div> 
 <?php
 }
 elseif($_SESSION['officer_Right'] == 1){
